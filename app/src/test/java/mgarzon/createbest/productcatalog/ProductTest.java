c30bb03f0721c59580c254dd6cf2f97681eafb00
Alexandre Ringuette <aring057@uottawa.ca>
2022-11-15 17:33:33 -0500

    Product test

---
+++ app/src/test/java/mgarzon/createbest/productcatalog/ProductTest.java
@@ -0,0 +1,22 @@
+package mgarzon.createbest.productcatalog;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class ProductTest {
+
+    @Test
+    public void checkProductName() {
+        Product aProduct = new Product( "DELL MONITOR", 180);
+        assertEquals("Check the name of the product", "DELL MONITOR", aProduct.getProductName());
+    }
+
+
+    @Test
+    public void checkProductPrice() {
+        // I am making this test to fail
+        Product aProduct = new Product( "DELL MONITOR", 180);
+        assertEquals("Check the price of the product", "0", aProduct.getPrice());
+    }
+
+
+}
